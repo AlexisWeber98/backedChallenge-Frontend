b606b24e34fb864ca3e487b4f5aba1d04c98cad8
Ale David Weber <Alexis.Weber@mailfence.com>
2024-04-08 14:44:02 -0300

    delete property to render and styles

--- src/components/Cards/Cards.tsx
+++ src/components/Cards/Cards.tsx
@@ -1,7 +1,7 @@
-import React from "react";
 import { useStore } from "../../store/products";
+import { useNavigate } from "react-router-dom";
 
-interface CardProps {
+export interface CardProps {
   id: string;
   title: string;
   description: string;
@@ -10,28 +10,26 @@ interface CardProps {
   category: string;
 }
 
-const Cards: React.FC<CardProps> = ({
-  id,
-  title,
-  description,
-  price,
-  category,
-  img,
-}) => {
-  const { deleteById } = useStore();
+const Cards: React.FC<CardProps> = ({ id, title, price, img }) => {
+  const { findbyId } = useStore();
+  const navigate = useNavigate();
+  const numberId = Number(id)
+
+  const handleNavigate = (id: string) => {
+    navigate(`/product/${id}`);
+   
+    findbyId(numberId);
+  };
+
   return (
-    <div className="card">
-      <h3>{title}</h3>
-      <p>{description}</p>
-      <p>{category}</p>
-      <h5>${price}</h5>
-      <img src={img} alt={title} />
-      <button
-        onClick={() => {
-          deleteById(id);
-        }}>
-        Delete this product
-      </button>
+    <div
+      onClick={() => handleNavigate(id)} 
+      className="bg-gray-500 border border-white m-3">
+      <p className="text-white">{title}</p>
+      <h5 className="text-white">${price}</h5>
+      <div className="content-center flex justify-around mt-4 p-2">
+        <img src={img} alt={title} className="rounded-[2rem]" />
+      </div>
     </div>
   );
 };
