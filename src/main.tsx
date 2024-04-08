d22e07fa9b17e7772ba1f866e47dfe74159daeae
Ale David Weber <Alexis.Weber@mailfence.com>
2024-04-08 14:44:26 -0300

    import styles tailwwind index.css

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import axios from "axios";
 import App from "./App";
-
+import "./index.css";
 
 axios.defaults.baseURL = "http://localhost:3001";
 
